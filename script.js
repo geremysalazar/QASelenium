84c332eacbab3e3313493818c7f5b1b4776c116b
Geremy Salazar <geremyjssalazar@hotmail.com>
2022-02-04 19:05:17 -0400

    install selenium web driver

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 var webdriver = require(‘selenium-webdriver’);
+
+var browser_name = new webdriver.Builder();
+
+withCapabilities(webdriver.Capabilities.chrome()).build();
+
+browser.get('https://techboss.com.co/');
